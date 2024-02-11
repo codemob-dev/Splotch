85c209cf08e590e49aefe2ee38c0d7c68fd1d6fb
BuildTools <unconfigured@null.spigotmc.org>
2024-02-10 19:39:48 -0500

    version bump

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -36,4 +36,4 @@ using System.Runtime.InteropServices;
 // X: major versions
 // Y: feature addition/ removal
 // Z: small patch/ bugfix
-[assembly: AssemblyVersion("0.2.1")]
+[assembly: AssemblyVersion("0.3.0")]
